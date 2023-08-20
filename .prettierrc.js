3236f17302cfe93ce4bf78490f85c7290ca4efec
Rortan <samaritanojr006@gmail.com>
2023-08-21 00:13:03 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    arrowParens: "always",
+    bracketSpacing: true,
+    bracketSameLine: true,
+    insertPragma: false,
+    proseWrap: "preserve",
+    quoteProps: "as-needed",
+    requirePragma: false,
+    semi: true,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    trailingComma: "es5",
+    useTabs: false,
+    printWidth: 100,
+    tabWidth: 4,
+    rangeStart: 0,
+    endOfLine: "auto",
+};
